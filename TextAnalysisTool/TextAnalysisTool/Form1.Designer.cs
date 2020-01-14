0a5689ea325734b8be7fb70c13a8583f65b1cf89
Murad <murad.haneya@gmail.com>
2020-01-14 22:55:57 +0000

    Init Form

---
+++ TextAnalysisTool/TextAnalysisTool/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TextAnalysisTool {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.browseButton = new System.Windows.Forms.Button();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.detailsGroup = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(543, 12);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(202, 13);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(335, 20);
+            this.pathTextBox.TabIndex = 1;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(317, 120);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(220, 20);
+            this.searchTextBox.TabIndex = 2;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(199, 36);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(75, 13);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "No file loaded.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(543, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // detailsGroup
+            // 
+            this.detailsGroup.Location = new System.Drawing.Point(202, 197);
+            this.detailsGroup.Name = "detailsGroup";
+            this.detailsGroup.Size = new System.Drawing.Size(200, 190);
+            this.detailsGroup.TabIndex = 5;
+            this.detailsGroup.TabStop = false;
+            this.detailsGroup.Text = "Word Information";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.detailsGroup);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.browseButton);
+            this.Name = "Form1";
+            this.Text = "Text Analysis Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox detailsGroup;
+    }
+}
+
