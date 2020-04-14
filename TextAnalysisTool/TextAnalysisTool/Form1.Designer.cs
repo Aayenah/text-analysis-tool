09fe1accc4bbe668ef5a05392525a57cc50dc04d
Murad <murad.haneya@gmail.com>
2020-04-14 16:10:46 +0100

    Implement search function

--- TextAnalysisTool/TextAnalysisTool/Form1.Designer.cs
+++ TextAnalysisTool/TextAnalysisTool/Form1.Designer.cs
@@ -32,14 +32,23 @@
             this.loadGroup = new System.Windows.Forms.GroupBox();
             this.filenameLabel = new System.Windows.Forms.Label();
             this.mainGroup = new System.Windows.Forms.GroupBox();
+            this.searchField = new System.Windows.Forms.TextBox();
+            this.sortOccurButton = new System.Windows.Forms.Button();
+            this.delWordButton = new System.Windows.Forms.Button();
             this.viewButton = new System.Windows.Forms.Button();
             this.editButton = new System.Windows.Forms.Button();
             this.wordsListLabel = new System.Windows.Forms.Label();
             this.wordsListBox = new System.Windows.Forms.ListBox();
-            this.delWordButton = new System.Windows.Forms.Button();
+            this.searchGroup = new System.Windows.Forms.GroupBox();
+            this.wordSearchLabel = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.occurSearchLabel = new System.Windows.Forms.Label();
+            this.timesLabel = new System.Windows.Forms.Label();
             this.statsGroup.SuspendLayout();
             this.loadGroup.SuspendLayout();
             this.mainGroup.SuspendLayout();
+            this.searchGroup.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // browseButton
@@ -125,6 +134,8 @@
             // 
             // mainGroup
             // 
+            this.mainGroup.Controls.Add(this.searchGroup);
+            this.mainGroup.Controls.Add(this.sortOccurButton);
             this.mainGroup.Controls.Add(this.delWordButton);
             this.mainGroup.Controls.Add(this.viewButton);
             this.mainGroup.Controls.Add(this.editButton);
@@ -136,6 +147,35 @@
             this.mainGroup.Size = new System.Drawing.Size(585, 328);
             this.mainGroup.TabIndex = 6;
             this.mainGroup.TabStop = false;
+            // 
+            // searchField
+            // 
+            this.searchField.Location = new System.Drawing.Point(93, 43);
+            this.searchField.Name = "searchField";
+            this.searchField.Size = new System.Drawing.Size(114, 20);
+            this.searchField.TabIndex = 5;
+            this.searchField.TextChanged += new System.EventHandler(this.searchField_TextChanged);
+            this.searchField.KeyUp += new System.Windows.Forms.KeyEventHandler(this.searchField_KeyUp);
+            // 
+            // sortOccurButton
+            // 
+            this.sortOccurButton.Location = new System.Drawing.Point(430, 206);
+            this.sortOccurButton.Name = "sortOccurButton";
+            this.sortOccurButton.Size = new System.Drawing.Size(149, 23);
+            this.sortOccurButton.TabIndex = 8;
+            this.sortOccurButton.Text = "Sort By Occurrence";
+            this.sortOccurButton.UseVisualStyleBackColor = true;
+            this.sortOccurButton.Click += new System.EventHandler(this.sortOccurButton_Click);
+            // 
+            // delWordButton
+            // 
+            this.delWordButton.Location = new System.Drawing.Point(430, 120);
+            this.delWordButton.Name = "delWordButton";
+            this.delWordButton.Size = new System.Drawing.Size(149, 23);
+            this.delWordButton.TabIndex = 7;
+            this.delWordButton.Text = "Delete";
+            this.delWordButton.UseVisualStyleBackColor = true;
+            this.delWordButton.Click += new System.EventHandler(this.delWordButton_Click);
             // 
             // viewButton
             // 
@@ -171,18 +211,67 @@
             this.wordsListBox.FormattingEnabled = true;
             this.wordsListBox.Location = new System.Drawing.Point(8, 33);
             this.wordsListBox.Name = "wordsListBox";
-            this.wordsListBox.Size = new System.Drawing.Size(416, 277);
+            this.wordsListBox.Size = new System.Drawing.Size(164, 277);
             this.wordsListBox.TabIndex = 5;
             // 
-            // delWordButton
+            // searchGroup
             // 
-            this.delWordButton.Location = new System.Drawing.Point(430, 120);
-            this.delWordButton.Name = "delWordButton";
-            this.delWordButton.Size = new System.Drawing.Size(149, 23);
-            this.delWordButton.TabIndex = 7;
-            this.delWordButton.Text = "Delete";
-            this.delWordButton.UseVisualStyleBackColor = true;
-            this.delWordButton.Click += new System.EventHandler(this.delWordButton_Click);
+            this.searchGroup.Controls.Add(this.timesLabel);
+            this.searchGroup.Controls.Add(this.occurSearchLabel);
+            this.searchGroup.Controls.Add(this.numericUpDown1);
+            this.searchGroup.Controls.Add(this.wordSearchLabel);
+            this.searchGroup.Controls.Add(this.searchField);
+            this.searchGroup.Location = new System.Drawing.Point(192, 19);
+            this.searchGroup.Name = "searchGroup";
+            this.searchGroup.Size = new System.Drawing.Size(213, 292);
+            this.searchGroup.TabIndex = 9;
+            this.searchGroup.TabStop = false;
+            this.searchGroup.Text = "Search";
+            // 
+            // wordSearchLabel
+            // 
+            this.wordSearchLabel.AutoSize = true;
+            this.wordSearchLabel.Location = new System.Drawing.Point(6, 46);
+            this.wordSearchLabel.Name = "wordSearchLabel";
+            this.wordSearchLabel.Size = new System.Drawing.Size(81, 13);
+            this.wordSearchLabel.TabIndex = 2;
+            this.wordSearchLabel.Text = "Search by word";
+            this.wordSearchLabel.Click += new System.EventHandler(this.wordSearchLabel_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(105, 83);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(49, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // occurSearchLabel
+            // 
+            this.occurSearchLabel.AutoSize = true;
+            this.occurSearchLabel.Location = new System.Drawing.Point(6, 85);
+            this.occurSearchLabel.Name = "occurSearchLabel";
+            this.occurSearchLabel.Size = new System.Drawing.Size(98, 13);
+            this.occurSearchLabel.TabIndex = 7;
+            this.occurSearchLabel.Text = "Occured more than";
+            // 
+            // timesLabel
+            // 
+            this.timesLabel.AutoSize = true;
+            this.timesLabel.Location = new System.Drawing.Point(160, 85);
+            this.timesLabel.Name = "timesLabel";
+            this.timesLabel.Size = new System.Drawing.Size(31, 13);
+            this.timesLabel.TabIndex = 8;
+            this.timesLabel.Text = "times";
             // 
             // Form1
             // 
@@ -201,6 +290,9 @@
             this.loadGroup.PerformLayout();
             this.mainGroup.ResumeLayout(false);
             this.mainGroup.PerformLayout();
+            this.searchGroup.ResumeLayout(false);
+            this.searchGroup.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -221,6 +313,13 @@
         private System.Windows.Forms.Button viewButton;
         private System.Windows.Forms.Button editButton;
         private System.Windows.Forms.Button delWordButton;
+        private System.Windows.Forms.Button sortOccurButton;
+        private System.Windows.Forms.TextBox searchField;
+        private System.Windows.Forms.GroupBox searchGroup;
+        private System.Windows.Forms.Label wordSearchLabel;
+        private System.Windows.Forms.Label occurSearchLabel;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label timesLabel;
     }
 }
 
--- TextAnalysisTool/TextAnalysisTool/Form1.cs
+++ TextAnalysisTool/TextAnalysisTool/Form1.cs
@@ -15,6 +15,10 @@ namespace TextAnalysisTool {
         OpenFileDialog ofd = new OpenFileDialog();
         char[] charsToTrim = { ' ', ',', '.', '?', ';'};
 
+        bool sortedByOccurrenceDesc = false;
+
+        List<Word> itemsList = new List<Word>();
+
 
         public Form1() {
             InitializeComponent();
@@ -58,13 +62,7 @@ namespace TextAnalysisTool {
                     }
                 }
 
-                wordsListBox.Sorted = true;
-
-                foreach (Word item in wordsListBox.Items)
-                {
-                    
-                }
-                
+                itemsList = wordsListBox.Items.OfType<Word>().ToList();
 
                 pathTextBox.Text = ofd.FileName;
                 filenameLabel.Text = ofd.SafeFileName;
@@ -118,13 +116,66 @@ namespace TextAnalysisTool {
 
         private void delWordButton_Click(object sender, EventArgs e)
         {
-            if(wordsListBox.SelectedItem != null)
+            if (wordsListBox.SelectedItem != null)
             {
                 Word w = (Word)wordsListBox.SelectedItem;
                 avl.RemoveItem(w);
                 wordsListBox.Items.Remove(w);
                 unqWordLabel.Text = "Unique words: " + avl.Count();
             }
+        }
+
+
+        
+        private void sortOccurButton_Click(object sender, EventArgs e)
+        {
+            if(sortedByOccurrenceDesc == false)
+            {
+                List<Word> descSortList = wordsListBox.Items.OfType<Word>().OrderByDescending(w => w.Occurrences).ToList();
+                wordsListBox.Items.Clear();
+                foreach (Word item in descSortList)
+                {
+                    wordsListBox.Items.Add(item);
+                    Console.WriteLine(item);
+                }
+                sortedByOccurrenceDesc = true;
+            }
+            else
+            {
+                List<Word> sortList = wordsListBox.Items.OfType<Word>().OrderBy(w => w.Occurrences).ToList();
+                wordsListBox.Items.Clear();
+                foreach (Word item in sortList)
+                {
+                    wordsListBox.Items.Add(item);
+                    Console.WriteLine(item);
+                }
+                sortedByOccurrenceDesc = false;
+            }
+            
+        }
+
+
+        private void searchField_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+        private void searchField_KeyUp(object sender, KeyEventArgs e)
+        {
+            wordsListBox.Items.Clear();
+            foreach (Word word in itemsList)
+            {
+                if (word.TheWord.StartsWith(searchField.Text, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    wordsListBox.Items.Add(word);
+                }
+            }
+        }
+
+        private void wordSearchLabel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
